45550bd4f4d8d59d89f7068b88069215e8bc0844
koooge <koooooge@gmail.com>
2018-02-07 15:13:35 +0000

    Add method post, put and delete

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -6,4 +6,22 @@ describe('index', () => {
       .expect('status', 200)
       .done(done);
   });
+
+  test('post /', (done) => {
+    frisby.get('http://localhost:3000/')
+      .expect('status', 200)
+      .done(done);
+  });
+
+  test('put /', (done) => {
+    frisby.get('http://localhost:3000/')
+      .expect('status', 200)
+      .done(done);
+  });
+
+  test('delete /', (done) => {
+    frisby.get('http://localhost:3000/')
+      .expect('status', 200)
+      .done(done);
+  });
 });
--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const fastify = require('fastify')()
 
 fastify.get('/', async (request, reply) => {
   reply.type('application/json').code(200);
-  return { hello: 'world' };
+  return { hello: 'get' };
+});
+
+fastify.post('/', async (request, reply) => {
+  reply.type('application/json').code(200);
+  return { hello: 'post' };
+});
+
+fastify.put('/', async (request, reply) => {
+  reply.type('application/json').code(200);
+  return { hello: 'put' };
+});
+
+fastify.delete('/', async (request, reply) => {
+  reply.type('application/json').code(200);
+  return { hello: 'delete' };
 });
 
 fastify.listen(3000, '127.0.0.1', function (err) {
